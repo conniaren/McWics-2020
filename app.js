e8ac4bb18acaa7dcc4ab3b3670ee6a9fb3d53f1e
conniaren <46227629+conniaren@users.noreply.github.com>
2020-01-18 22:45:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, Button } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import SafeAreaView from 'react-native-safe-area-view';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+});
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
+});
 const App = createAppContainer(MainNavigator);
 export default App;
 class HomeScreen extends React.Component {
