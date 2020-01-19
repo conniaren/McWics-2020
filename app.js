5fb17f8481ea873a28bc6fa13627fdeb1eeafe36
conniaren <46227629+conniaren@users.noreply.github.com>
2020-01-18 23:52:19 -0500

    Update app.js
    entire updated app code home screen

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
-const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Profile: {screen: ProfileScreen},
-});
-const App = createAppContainer(MainNavigator);
-export default App;
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.navigate('Profile')}
+        />
+      </View>
+    );
+  }
+}
 class HomeScreen extends React.Component {
   static navigationOptions={
     title:'Welcome',
   };
   _onPressButton() {
-    alert('You tapped the button!')
+    //alert('You tapped the button!')
+    this.props.navigation.navigate('Profile')
   }
   render() {
-    const {navigate} = this.props.navigation;
+    //const {navigate} = this.props.navigation;
     return (
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimensions, so the children can't expand.
@@ -44,6 +52,125 @@ class HomeScreen extends React.Component {
     );
   }
 }
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen, },
+  Profile: {screen: ProfileScreen},
+},
+{
+  initialRouteName:'Home',
+}
+);
+/*export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello World!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+      
+    );
+  }
+}*/
+/*export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={styles.container}>
+      <Image source={pic} style={{width: 400, height: 217}}/>
+      </View>
+    );
+  }
+}*/
+
+/*class Greeting extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}*/
+/*class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}*/
+
+/*const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View >
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigBlue}>just bigBlue</Text>
+        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+      </View>
+    );
+  }
+}*/
+const App = createAppContainer(MainNavigator);
+export default App;
+
 
 
 const styles = StyleSheet.create({
